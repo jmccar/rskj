cb4781d9495d408e945a07958adaf23ecdbbe504
Martín Coll <martingonzalezcoll@gmail.com>
2019-03-14 18:40:32 -0300

    Rename TrieImpl to Trie

--- rskj-core/src/test/java/co/rsk/peg/BridgeTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeTest.java
@@ -39,7 +39,7 @@ import co.rsk.peg.exception.InvalidMerkleBranchException;
 import co.rsk.peg.whitelist.OneOffWhiteListEntry;
 import co.rsk.peg.whitelist.UnlimitedWhiteListEntry;
 import co.rsk.test.World;
-import co.rsk.trie.TrieImpl;
+import co.rsk.trie.Trie;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.config.BlockchainConfig;
 import org.ethereum.config.BlockchainNetConfig;
@@ -2615,6 +2615,6 @@ public class BridgeTest {
     }
 
     public static RepositoryImpl createRepositoryImpl(RskSystemProperties config) {
-        return new RepositoryImpl(new TrieImpl(null, true), new HashMapDB(), new TrieStorePoolOnMemory(), config.detailsInMemoryStorageLimit());
+        return new RepositoryImpl(new Trie(null, true), new HashMapDB(), new TrieStorePoolOnMemory(), config.detailsInMemoryStorageLimit());
     }
 }
